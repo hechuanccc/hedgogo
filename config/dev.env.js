2631820ac0e95e936b5bf6fc69b471e2e942d33f
Dean Christian <dean@unnotech.com>
2017-12-09 15:40:21 +0800

    quick revert

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,5 +1,8 @@
-module.exports = {
+var merge = require('webpack-merge')
+var prodEnv = require('./prod.env')
+
+module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   HOST: '"http://a546542.eastasia.cloudapp.azure.com:8000"',
   NAME: '"Lottery"'
-}
+})
