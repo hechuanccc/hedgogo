6599df79756b8da68ff2a67a7eef756d3e81b95b
raywu0406 <ray@unnotech.com>
2017-12-26 14:34:50 +0800

    no message

--- src/views/bill/remit.vue
+++ src/views/bill/remit.vue
@@ -20,7 +20,7 @@
                             <option value="5">{{$t('status.declined')}}</option>
                         </select>
                         <level class="inline" :level="member_level" @level-select="changeFromLevel"></level>
-                        <input type="text" v-model.trim="query.transaction_id" class="form-control w-sm" v-bind:placeholder="$t('bill.order_id')"/>
+                        <input type="number" v-model.trim="query.transaction_id" class="form-control w-sm" v-bind:placeholder="$t('bill.order_id')"/>
                         <input type="text" v-model="query.member_q" class="form-control w-sm" v-bind:placeholder="$t('common.member')" />
                         <input type="text" v-model="query.updated_by" class="form-control inline w-sm" v-bind:placeholder="$t('common.operator')" />
                         <input type="text" class="form-control inline w-sm" v-model="query.real_name_q" v-bind:placeholder="$t('common.real_name')" />
