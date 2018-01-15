4b5103475cc05e2e8d3c14ef796ea77aaf057de9
raywu0406 <ray@unnotech.com>
2018-01-15 13:56:18 +0800

    no message

--- src/views/overview.vue
+++ src/views/overview.vue
@@ -82,7 +82,7 @@ export default {
                         backgroundColor: `hsl(${this.color[title]}, 88%, 80%)`,
                         borderColor: `hsl(${this.color[title]}, 88%, 55%)`,
                         borderWidth: 2,
-                        data: data.map(e => e[title].toFixed(3))
+                        data: data.map(e => e[title].toFixed(2))
                     }]
                 })
                 this.$set(this.options, title, {
