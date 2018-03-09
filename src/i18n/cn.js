e92665a862d6961cff99f4a1be66284acc18cded
raywu0406 <ray@unnotech.com>
2018-03-09 14:24:01 +0800

    [Game History Detail Page] Based on new `official_invalid` and  `valid` result status, handle it and add message to inform user.

--- src/i18n/cn.js
+++ src/i18n/cn.js
@@ -617,7 +617,8 @@ export default {
         leopard: '豹子',
         prime: '质',
         composite: '合',
-        ball_max_min_diff: '跨度'
+        ball_max_min_diff: '跨度',
+        official_invalid: '官方开奖无效'
     },
     agent: {
         number: '编号',
