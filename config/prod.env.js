1dcfa299e2d9fa14e13e8d99035de3353e1a4166
raywu0406 <ray@unnotech.com>
2018-03-22 15:47:44 +0800

    [Production Config.] Console log environment config.

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,3 +1,7 @@
+console.log('process.env.HOST: ', process.env.HOST)
+console.log('HOST: ', '"' + process.env.HOST + '"')
+console.log('process.env.chatApi: ', process.env.chatApi)
+console.log('CHATHOST: ', '"' + process.env.chatApi + '"')
 module.exports = {
   NODE_ENV: '"production"',
   HOST: '"' + process.env.HOST + '"',
