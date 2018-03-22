51d7d700eb88a6c651c415ef1f4d70fe122fc671
raywu0406 <ray@unnotech.com>
2018-03-22 16:21:04 +0800

    [Robot Management Page] Hide this page after configs are set.

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,7 +1,3 @@
-console.log('process.env.HOST: ', process.env.HOST)
-console.log('HOST: ', '"' + process.env.HOST + '"')
-console.log('process.env.chatApi: ', process.env.chatApi)
-console.log('CHATHOST: ', '"' + process.env.chatApi + '"')
 module.exports = {
   NODE_ENV: '"production"',
   HOST: '"' + process.env.HOST + '"',
