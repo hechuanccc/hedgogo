0b018b7ca5a535505927d1c46938e94ca2f18fd9
Sammy Wen <sammy.wen@unnotech.com>
2018-07-14 08:10:50 +0000

    dev.env.js edited online with Bitbucket

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,7 +3,7 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  HOST: '"http://a546542.eastasia.cloudapp.azure.com:8000"',
-  CHATHOST: '"http://a546542.eastasia.cloudapp.azure.com:8003"',  
+  HOST: '"http://b583747.eastasia.cloudapp.azure.com:8000"',
+  CHATHOST: '"http://b583747.eastasia.cloudapp.azure.com:8003"',  
   NAME: '"Lottery"'
 })
